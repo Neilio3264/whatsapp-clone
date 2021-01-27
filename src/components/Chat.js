d49b4b48e115b920aa12bf4e1778743b2917a548
Neilio3264 <neilrathod12@gmail.com>
2021-01-26 23:07:31 -0600

    Add TODO for test commit

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Chat.css";
 
+// TODO: Build the chat section of the app
 function Chat() {
   return (
     <div className="chat">
-      <h1>I am a chat</h1>
+      <h1>I am a chat. This is next</h1>
     </div>
   );
 }
